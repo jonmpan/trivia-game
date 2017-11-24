7bf6a1501b981fb85e08f0798e02b742bc57d35a
Jon Pan <jonmpan@gmail.com>
2017-11-23 18:24:03 -0800

    v1.02 fixed some issues

--- js/questions.js
+++ js/questions.js
@@ -256,8 +256,8 @@ q32Obj = {
 }
 q33Obj = {
 	q: '|1-2-1-2-1-2-1+1|=?',
-	a1: '9999999',
-	a2: '0',
+	a1: '121212211',
+	a2: '112122112',
 	a3: '9',
 	a4: '⑨',
 	a: 'answer4',
@@ -265,10 +265,10 @@ q33Obj = {
 q34Obj = {
 	q: '|1-2-1-2-1+2-1|=?',
 	a1: '1211212',
-	a2: '1121212',
-	a3: '1211122',
+	a2: '1211122',
+	a3: '9',
 	a4: '⑨',
-	a: 'answer3',
+	a: 'answer2',
 }
 q35Obj = {
 	q: '|23-15+2|=?',
@@ -278,6 +278,286 @@ q35Obj = {
 	a4: '⑨',
 	a: 'answer1',
 }
+q36Obj = {
+	q: '1+4+5+4-3=?',
+	a1: '31545',
+	a2: '31454',
+	a3: '11',
+	a4: '⑨',
+	a: 'answer2',
+}
+q37Obj = {
+	q: '5-2-3+6=?',
+	a1: '3652',
+	a2: '6',
+	a3: '3256',
+	a4: '⑨',
+	a: 'answer3',
+}
+q38Obj = {
+	q: '27-19=?',
+	a1: '1927',
+	a2: '2719',
+	a3: '8',
+	a4: '⑨',
+	a: 'answer3',
+}
+q39Obj = {
+	q: '|5-8|+6=?',
+	a1: '856',
+	a2: '586',
+	a3: '865',
+	a4: '⑨',
+	a: 'answer4',
+}
+q40Obj = {
+	q: '17-|8-15|=?',
+	a1: '15817',
+	a2: '17815',
+	a3: '81517',
+	a4: '⑨',
+	a: 'answer1',
+}
+q41Obj = {
+	q: '9-6-5=?',
+	a1: '-2',
+	a2: '-3',
+	a3: '569',
+	a4: '⑨',
+	a: 'answer1',
+}
+q42Obj = {
+	q: '3+9+7=?',
+	a1: '21',
+	a2: '19',
+	a3: '397',
+	a4: '⑨',
+	a: 'answer2',
+}
+q43Obj = {
+	q: '12+1+1+1+1=?',
+	a1: '1211111',
+	a2: '1111111',
+	a3: '121111',
+	a4: '⑨',
+	a: 'answer3',
+}
+q44Obj = {
+	q: '15-1-1-1-1-1-1=?',
+	a1: '11111115',
+	a2: '1111115',
+	a3: '111115',
+	a4: '⑨',
+	a: 'answer4',
+}
+q45Obj = {
+	q: '7-71-17=?',
+	a1: '17717',
+	a2: '17177',
+	a3: '17771',
+	a4: '⑨',
+	a: 'answer1',
+}
+q46Obj = {
+	q: '456-123+78=?',
+	a1: '45612378',
+	a2: '12345678',
+	a3: '45678123',
+	a4: '⑨',
+	a: 'answer2',
+}
+q47Obj = {
+	q: '78-456-123=?',
+	a1: '12378456',
+	a2: '12346578',
+	a3: '12345678',
+	a4: '⑨',
+	a: 'answer3',
+}
+q48Obj = {
+	q: '123-456+342',
+	a1: '342123456',
+	a2: '456123342',
+	a3: '342456123',
+	a4: '⑨',
+	a: 'answer4',
+}
+q49Obj = {
+	q: '321-654-87=?',
+	a1: '87654321',
+	a2: '87645321',
+	a3: '32165487',
+	a4: '⑨',
+	a: 'answer1',
+}
+q50Obj = {
+	q: '87+654+321=?',
+	a1: '87654231',
+	a2: '87654321',
+	a3: '87564321',
+	a4: '⑨',
+	a: 'answer2',
+}
+q51Obj = {
+	q: '0+0+7=?',
+	a1: '7',
+	a2: 'James Bond',
+	a3: '007',
+	a4: '⑨',
+	a: 'answer3',
+}
+q52Obj = {
+	q: '0-3+12=?',
+	a1: '3012',
+	a2: '312',
+	a3: '15',
+	a4: '⑨',
+	a: 'answer4',
+}
+q53Obj = {
+	q: '7-0-0=?',
+	a1: '007',
+	a2: '700',
+	a3: 'James Bond',
+	a4: '⑨',
+	a: 'answer1',
+}
+q54Obj = {
+	q: '0+0-0+0-0=?',
+	a1: '0',
+	a2: '00000',
+	a3: 'False',
+	a4: '⑨',
+	a: 'answer2',
+}
+q55Obj = {
+	q: '|0-1|=?',
+	a1: '1',
+	a2: '0',
+	a3: '10',
+	a4: '⑨',
+	a: 'answer3',
+}
+q56Obj = {
+	q: 'Cirno=?',
+	a1: '9',
+	a2: 'Smart',
+	a3: 'Good at math',
+	a4: '⑨',
+	a: 'answer4',
+}
+q57Obj = {
+	q: '9+0-3-0=?',
+	a1: '6',
+	a2: '0390',
+	a3: '390',
+	a4: '⑨',
+	a: 'answer1',
+}
+q58Obj = {
+	q: '15-9-0=?',
+	a1: '0915',
+	a2: '6',
+	a3: '9',
+	a4: '⑨',
+	a: 'answer2',
+}
+q59Obj = {
+	q: '1+0+1+Dalmations=?',
+	a1: '11',
+	a2: '11 Dalmations',
+	a3: '101Dalmations',
+	a4: '⑨',
+	a: 'answer3',
+}
+q60Obj = {
+	q: 'G+E+O+R+G+E=?',
+	a1: 'Amazing TA',
+	a2: 'GEORGE',
+	a3: 'Very Helpful',
+	a4: '⑨',
+	a: 'answer2',
+}
+q61Obj = {
+	q: '63-54=?',
+	a1: '5463',
+	a2: '6354',
+	a3: '6453',
+	a4: '⑨',
+	a: 'answer4',
+}
+q62Obj = {
+	q: 'E-G-R-O-E-G=?',
+	a1: 'GEORGE',
+	a2: 'Great TA!',
+	a3: 'EGROEG',
+	a4: '⑨',
+	a: 'answer1',
+}
+q63Obj = {
+	q: '(1+1)-(2+2)=?',
+	a1: '11',
+	a2: '2211',
+	a3: '1212',
+	a4: '⑨',
+	a: 'answer2',
+}
+q64Obj = {
+	q: '(5-3)-(7-2)=?',
+	a1: '3527',
+	a2: '7352',
+	a3: '2735',
+	a4: '⑨',
+	a: 'answer3',
+}
+q65Obj = {
+	q: '(2+0)-(1+1)=?',
+	a1: '1201',
+	a2: '2011',
+	a3: '1120',
+	a4: '⑨',
+	a: 'answer4',
+}
+q66Obj = {
+	q: '(10-9)-(6+4)=?',
+	a1: '-9',
+	a2: '64910',
+	a3: '69104',
+	a4: '⑨',
+	a: 'answer1',
+}
+q67Obj = {
+	q: '(1+2+3)+(1-2-3)=?',
+	a1: '123123',
+	a2: '123321',
+	a3: '321123',
+	a4: '⑨',
+	a: 'answer2',
+}
+q68Obj = {
+	q: '(2+3)-(4+5)-(6+7)=?',
+	a1: '765432',
+	a2: '234567',
+	a3: '674523',
+	a4: '⑨',
+	a: 'answer3',
+}
+q69Obj = {
+	q: '(6+7)-(2+2)=?',
+	a1: '2267',
+	a2: '6722',
+	a3: '2672',
+	a4: '⑨',
+	a: 'answer4',
+}
+q70Obj = {
+	q: 'J+S+O+N=?',
+	a1: 'JSON',
+	a2: 'Jayson',
+	a3: 'JS Object Notation',
+	a4: '⑨',
+	a: 'answer1',
+}
 
 questions = [q1Obj, q2Obj, q3Obj, q4Obj, q5Obj, q6Obj, q7Obj, q8Obj, q9Obj, q10Obj, q11Obj, q12Obj, q13Obj, q14Obj, q15Obj, q16Obj, q17Obj, q18Obj, q19Obj, q20Obj];
-lunaticquestions = [q1Obj, q2Obj, q3Obj, q4Obj, q5Obj, q6Obj, q7Obj, q8Obj, q9Obj, q10Obj, q11Obj, q12Obj, q13Obj, q14Obj, q15Obj, q16Obj, q17Obj, q18Obj, q19Obj, q20Obj, q21Obj, q22Obj, q23Obj, q24Obj, q25Obj, q26Obj, q27Obj, q28Obj, q29Obj, q30Obj, q31Obj, q32Obj, q33Obj, q34Obj, q35Obj];
+lunaticquestions = [q1Obj, q2Obj, q3Obj, q4Obj, q5Obj, q6Obj, q7Obj, q8Obj, q9Obj, q10Obj, q11Obj, q12Obj, q13Obj, q14Obj, q15Obj, q16Obj, q17Obj, q18Obj, q19Obj, q20Obj, q21Obj, q22Obj, q23Obj, q24Obj, q25Obj, q26Obj, q27Obj, q28Obj, q29Obj, q30Obj, q31Obj, q32Obj, q33Obj, q34Obj, q35Obj, q36Obj, q37Obj, q38Obj, q39Obj, q40Obj, q41Obj, q42Obj, q43Obj, q44Obj, q45Obj, q46Obj, q47Obj, q48Obj, q49Obj, q50Obj, q51Obj, q52Obj, q53Obj, q54Obj, q55Obj, q56Obj, q57Obj, q58Obj, q59Obj, q60Obj, q61Obj, q62Obj, q63Obj, q64Obj, q65Obj, q66Obj, q67Obj, q68Obj, q69Obj, q70Obj];
